0288ff0652ba464b9412323281b477eee8b22916
Dan Debrunner <djd@debrunners.com>
2019-09-29 23:09:24 +0000

    Don't hide output

--- builds/nginx_njs/get_ngx_njs.sh
+++ builds/nginx_njs/get_ngx_njs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'Building dockerfile'
-docker build -t karstenf/endpoint_monitor:latest . > /dev/null 2>&1
+docker build -t karstenf/endpoint_monitor:latest . 
 echo 'Running dockerfile'
 docker run --name endpointContainerKF -d karstenf/endpoint_monitor:latest
 sleep 40s
@@ -11,7 +11,7 @@ while [ $RUNING = true ]; do
     RUNING=$(docker inspect -f '{{.State.Running}}' endpointContainerKF)
 done
 docker cp endpointContainerKF:/nginx-1.14.1/objs/ngx_http_js_module.so .
-docker rm endpointContainerKF > /dev/null 2>&1
+docker rm endpointContainerKF 
 if [ -f ./ngx_http_js_module.so ]; then
     echo 'Container removed, ngx_http_js_module.so file copied to current directory'
 else
