c169925d4fe58f42f874a9591d90637188d60635
Dan Debrunner <djd@debrunners.com>
2019-09-29 23:40:53 +0000

    RUn docker container in foreground

--- builds/nginx_njs/get_ngx_njs.sh
+++ builds/nginx_njs/get_ngx_njs.sh
@@ -2,14 +2,7 @@
 echo 'Building dockerfile'
 docker build -t karstenf/endpoint_monitor:latest . 
 echo 'Running dockerfile'
-docker run --name endpointContainerKF -d karstenf/endpoint_monitor:latest
-sleep 40s
-RUNING=$(docker inspect -f '{{.State.Running}}' endpointContainerKF)
-while [ $RUNING = true ]; do
-    echo 'Container not finished running, sleeping for 10 seconds'
-    sleep 10s
-    RUNING=$(docker inspect -f '{{.State.Running}}' endpointContainerKF)
-done
+docker run --name endpointContainerKF karstenf/endpoint_monitor:latest
 docker cp endpointContainerKF:/nginx-1.14.1/objs/ngx_http_js_module.so .
 docker rm endpointContainerKF 
 if [ -f ./ngx_http_js_module.so ]; then
