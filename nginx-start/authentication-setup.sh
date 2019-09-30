08807d3df591a969389ae37552f5d5573ba7ae68
Dan Debrunner <djd@debrunners.com>
2019-09-30 18:24:11 +0000

    Fix script

--- nginx-start/authentication-setup.sh
+++ nginx-start/authentication-setup.sh
@@ -1,5 +1,5 @@
 SECRET_HTACCESS=/var/run/secrets/streams-endpoint-monitor/server-auth/.htaccess
-if  -f "${SECRET_HTACCESS}" ]; then
+if [ -f "${SECRET_HTACCESS}" ]; then
     /bin/cp \
         /opt/app-root/src/nginx-optional-cfg/auth_basic.conf \
         /var/opt/streams-endpoint-monitor/authentication.basic.conf
