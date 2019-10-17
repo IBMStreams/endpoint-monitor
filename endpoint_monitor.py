8cec046f6e42620e30827eb6fa14a7214bd6f485
Karsten12 <kfonseca@berkeley.edu>
2019-10-17 14:43:48 -0700

    fix function name2

--- endpoint_monitor.py
+++ endpoint_monitor.py
@@ -56,74 +56,74 @@ def _job_new_incarnation(job):
     return EndpointJob(name, generationId, applicationName, servers, ops, pes, ops_in_pe)
 
 
-    def _job_update(job_info, j):
-        """ Function that efficiently checks and updates job_info's details according to following conditions:
+def _job_update(job_info, j):
+    """ Function that efficiently checks and updates job_info's details according to following conditions:
 
-        For each pe, check if job_info's pe launchCounts are the same as j's
-            if same -> Check if any of those ops (in that pe) have an existing server
-                if existing server, don't do anything
-                if no existing server, PE is just starting up, check for new server, and if so add it
-            if not same -> Check if PE's server is back up
-                if back up, add it and update PE launchCount, then remove old invalid servers
-                if not back up, don't do anything (need to wait for server to come back up)
+    For each pe, check if job_info's pe launchCounts are the same as j's
+        if same -> Check if any of those ops (in that pe) have an existing server
+            if existing server, don't do anything
+            if no existing server, PE is just starting up, check for new server, and if so add it
+        if not same -> Check if PE's server is back up
+            if back up, add it and update PE launchCount, then remove old invalid servers
+            if not back up, don't do anything (need to wait for server to come back up)
 
-        Arguments:
-            job_info {EndpointJob} -- Existing job that we want to check and update if neccessary
-            j {job} -- Streams job object obtained from instance.get_jobs()
+    Arguments:
+        job_info {EndpointJob} -- Existing job that we want to check and update if neccessary
+        j {job} -- Streams job object obtained from instance.get_jobs()
 
-        Returns:
-            {EndpointJob} -- A new, updated EndpointJob object if we have any new servers to add, or job_info if no update required
-        """
-        servers_to_add = set()
-        pes_changed = []
+    Returns:
+        {EndpointJob} -- A new, updated EndpointJob object if we have any new servers to add, or job_info if no update required
+    """
+    servers_to_add = set()
+    pes_changed = []
 
-        for pe in j.get_pes():
-            # Get the names of all the rest operators in this PE, use get bc some pes might not have any rest operators
-            op_names = job_info.ops_in_pe.get(pe.id)
-            if op_names is None:
-                # PE does not contain any rest operators, thus don't care about it, go onto next PE
-                continue
-            if job_info.pes[pe.id] == pe.launchCount:
-                print("SAME LAUNCHCOUNT")
-                # Check if this PE has any existing servers
-                if not _check_if_server_in_pe(job_info, pe.id):
-                    # PE launchCount same, and no servers in this PE, thus server just starting up, check if it is up and running
-                    for op_name in op_names:
-                        op_obj = j.get_operators(op_name)
-                        if op_obj:
-                            new_server = _get_server_address(op_obj[0], pe)
-                            if new_server:
-                                # New server is up and running, add it, assumes 1 server/PE (even if more than 1 rest operator in a PE)
-                                servers_to_add.add(new_server)
-                                break
-            else:
-                print("DIFFERENT LAUNCHCOUNT")
-                # PE launchCount different, thus PE restarted, iterate through the op names in this PE
-                # For each op name, get the actual op object and check if new server is up
+    for pe in j.get_pes():
+        # Get the names of all the rest operators in this PE, use get bc some pes might not have any rest operators
+        op_names = job_info.ops_in_pe.get(pe.id)
+        if op_names is None:
+            # PE does not contain any rest operators, thus don't care about it, go onto next PE
+            continue
+        if job_info.pes[pe.id] == pe.launchCount:
+            print("SAME LAUNCHCOUNT")
+            # Check if this PE has any existing servers
+            if not _check_if_server_in_pe(job_info, pe.id):
+                # PE launchCount same, and no servers in this PE, thus server just starting up, check if it is up and running
                 for op_name in op_names:
                     op_obj = j.get_operators(op_name)
                     if op_obj:
                         new_server = _get_server_address(op_obj[0], pe)
                         if new_server:
-                            # New server is up and running, add it, assumes 1 server/PE (even if more than 1 rest operator in a PE), update PE launchCount
+                            # New server is up and running, add it, assumes 1 server/PE (even if more than 1 rest operator in a PE)
                             servers_to_add.add(new_server)
-                            job_info.pes[pe.id] = pe.launchCount
-                            # Add the PE to the list, so we can find & remove all the old invalid servers that have this pe id
-                            pes_changed.append(pe.id)
                             break
+        else:
+            print("DIFFERENT LAUNCHCOUNT")
+            # PE launchCount different, thus PE restarted, iterate through the op names in this PE
+            # For each op name, get the actual op object and check if new server is up
+            for op_name in op_names:
+                op_obj = j.get_operators(op_name)
+                if op_obj:
+                    new_server = _get_server_address(op_obj[0], pe)
+                    if new_server:
+                        # New server is up and running, add it, assumes 1 server/PE (even if more than 1 rest operator in a PE), update PE launchCount
+                        servers_to_add.add(new_server)
+                        job_info.pes[pe.id] = pe.launchCount
+                        # Add the PE to the list, so we can find & remove all the old invalid servers that have this pe id
+                        pes_changed.append(pe.id)
+                        break
 
-        # After checking all PE's, if any new servers, remove old invalid ones and update job's servers
-        if servers_to_add:
-            # Remove the old servers where the PE's launchCounts have changed
-            servers_to_remove = set([x for x in job_info.servers if x.pe_id in pes_changed])
-            valid_servers = job_info.servers - servers_to_remove
-            # Add the new servers
-            new_servers = valid_servers.union(servers_to_add)
-            # Update the job w/ the new info
-            return EndpointJob(job_info.name, job_info.generationId, job_info.applicationName, new_servers, job_info.ops, job_info.pes, job_info.ops_in_pe)
+    # After checking all PE's, if any new servers, remove old invalid ones and update job's servers
+    if servers_to_add:
+        # Remove the old servers where the PE's launchCounts have changed
+        servers_to_remove = set([x for x in job_info.servers if x.pe_id in pes_changed])
+        valid_servers = job_info.servers - servers_to_remove
+        # Add the new servers
+        new_servers = valid_servers.union(servers_to_add)
+        # Update the job w/ the new info
+        return EndpointJob(job_info.name, job_info.generationId, job_info.applicationName, new_servers, job_info.ops, job_info.pes, job_info.ops_in_pe)
 
-        # PE's may or may not have restarted, but no new servers are up, thus don't update job, don't change config
-        return job_info
+    # PE's may or may not have restarted, but no new servers are up, thus don't update job, don't change config
+    return job_info
 
 def _check_if_server_in_pe(job_info, pe_id):
     servers = [server for server in job_info.servers if server.pe_id == pe_id]
