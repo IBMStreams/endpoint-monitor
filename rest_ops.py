e6b4a4dcf2037162027d6e7018049c25141f5953
Dan Debrunner <djd@debrunners.com>
2019-10-23 18:15:38 +0000

    Saved the exposed ports

--- rest_ops.py
+++ rest_ops.py
@@ -12,10 +12,12 @@ def server_url(server):
 def find_contexts(server, url, client_cert):
     oppaths=set()
     contexts=set()
+    exposed_ports = None
     ports_url = url + 'ports/info'
     ports = requests.get(ports_url, cert=client_cert, verify=False).json()
     if 'exposedPorts' in ports:
-        for port in ports['exposedPorts']:
+        exposed_ports = ports['exposedPorts']
+        for port in exposed_ports:
             cps = port['contextPaths']
             for id_ in cps:
                 cp = cps[id_].replace('\\', '')
@@ -23,13 +25,13 @@ def find_contexts(server, url, client_cert):
                 contexts.add(scp[1])
                 oppaths.add(scp[1]+'/'+scp[2])
 
-    return contexts, oppaths
+    return contexts, oppaths, exposed_ports
 
 
 def fill_in_details(endjob, client_cert):
     for server in endjob.servers:
         url = server_url(server)
         contexts, paths = find_contexts(server, url, client_cert)
-        endjob.server_details[server] = endpoint_monitor.ServerDetail(url, contexts, paths)
+        endjob.server_details[server] = endpoint_monitor.ServerDetail(url, contexts, paths, exposed_ports)
         print('Server', server)
         print('ServerDetail', endjob.server_details[server])
