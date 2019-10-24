ebb0407bfc5c1810f87832565667db4a42b27b20
Dan Debrunner <djd@debrunners.com>
2019-10-24 17:10:47 +0000

    Get aliases for inject

--- rest_ops.py
+++ rest_ops.py
@@ -35,6 +35,31 @@ def _find_contexts(server, url, client_cert):
 
     return contexts, oppaths, exposed_ports
 
+def _make_port_alias(path, port, output=True)
+    r = '/ports/'
+    r += 'output' if output else 'input'
+    r += '/'
+    r += str(port)
+    r += '/'
+    alias = path.replace(r)
+    if alias != path:
+        return path
+
+def _add_alias(aliases, path, port, output=True):
+    alias = _make_port_alias(inject, 0)
+    if alias:
+        aliases[alias] = inject
+
+def _create_aliases(ports):
+    aliases = {}
+    for port in ports:
+        kind = port['operatorKind']
+        if kind == 'com.ibm.streamsx.inet.rest::HTTPJsonInject':
+            cps = port['contextPaths']
+            _add_alias(aliases, cps['inject'], 0)
+
+    return aliases
+    
 #
 # Fills in the details for a given server to return
 # a ServerDetail tuple.
@@ -43,6 +68,8 @@ def fill_in_details(endjob, client_cert):
     for server in endjob.servers:
         url = server_url(server)
         contexts, paths, ports = _find_contexts(server, url, client_cert)
+        aliases = _create_aliases(ports)
+        print('Aliases', aliases)
         endjob.server_details[server] = endpoint_monitor.ServerDetail(url, contexts, paths, ports)
         print('Server', server)
         print('ServerDetail', endjob.server_details[server])
